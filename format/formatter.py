c1c4796a2d2ed20f9fb7eec146e17f9a931446ac
Reuben <reubendutton@gmail.com>
2021-12-10 13:50:24 +1000

    Removed caching'

--- format/formatter.py
+++ format/formatter.py
@@ -9,10 +9,6 @@ class Formatter:
 
 	def __init__(self, frame: Frame):
 		self.frame = frame
-		self.cachedColours = []
-		self.cachedCleanImage = None
-		self.cachedFullImage = None
-		self.cached = False
 		self.painter = Painter(frame)
 		self.printer = Printer(frame)
 
@@ -20,22 +16,8 @@ class Formatter:
 		self.frame = frame
 		self.painter.setFrame(frame)
 		self.printer.setFrame(frame)
-		self.cached = False
-
-	def isCached(self, colours: list[tuple[int, int, int]]):
-		return self.cachedColours == colours and self.cached
-
-	def cache(self, colours: list[tuple[int, int, int]], cleanImage, fullImage):
-		self.cachedCleanImage = cleanImage 
-		self.cachedFullImage = fullImage
-		self.cachedColours = colours
-		self.cached = True
 
 	def getImages(self, colours: list[tuple[int, int, int]]):
-		if self.isCached(colours):
-			return self.cachedCleanImage, self.cachedFullImage
-
 		cleanImage = self.painter.paintColours(colours)
 		fullImage = self.printer.printNames(cleanImage)
-		self.cache(colours, cleanImage, fullImage)
 		return cleanImage, fullImage
